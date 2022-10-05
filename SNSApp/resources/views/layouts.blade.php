73484f6fb5e4e2523ac6ebcb4eca7edcc7ae70fc
NodashZ <nohsyu@me.com>
2022-10-05 18:02:57 +0900

    プロジェクト追加

---
+++ SNSApp/resources/views/layouts.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="csrf-token" content="{{csrf_token()}}">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script>
+    window.api_token = "{{ \Auth::user()->api_token ?? null}}";
+  </script>
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
+  <script src="/js/app.js" defer></script>
+  <title>{{env('APP_NAME')}}</title>
+</head>
+
+<body>
+  <div id="app">
+    <header>
+      <header-component appname="{{env('APP_NAME')}}"></header-component>
+    </header>
+    <br>
+    <div class="container">
+      @yield('content')
+    </div>
+    
+  </div>
+</body>
+
+</html>
